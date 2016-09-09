9095179c41e4ca80d492aecbe752b0a2f49a4ba6
tklem <tklem2@illinois.edu>
2016-09-08 23:29:45 -0500

    Revised README

--- createTable.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function # Python 2/3 compatibility
-import boto3
-
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
-
-print(dynamodb)
